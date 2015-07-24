afb144f38ddb542a14137685c270b83d2c51b6d9
Marvin Qian <fueledbymarvin@gmail.com>
2015-07-24 02:15:24 -0400

    Destroy perishable after download
    TODO: Background job to clean files since don't want it to block for
    large files.

--- app/controllers/v1/perishables_controller.rb
+++ app/controllers/v1/perishables_controller.rb
@@ -1,20 +1,23 @@
 class V1::PerishablesController < V1::BaseController
   def show
-    perishable = Perishable.find_by id: params[:id]
+    @perishable = Perishable.find_by id: params[:id]
 
-    if perishable
-      send_file perishable.document.path, type: perishable.document.content_type
+    if @perishable
+      File.open(@perishable.document.path, 'r') do |f|
+        send_data f.read, type: @perishable.document.content_type
+      end
+      @perishable.destroy
     else
       render json: { error: 'not found' }, status: :not_found
     end
   end
 
   def create
-    perishable = Perishable.new perishable_params
-    if perishable.save
-      render json: perishable, status: :created
+    @perishable = Perishable.new perishable_params
+    if @perishable.save
+      render json: @perishable, status: :created
     else
-      render json: perishable.errors, status: :unprocessable_entity
+      render json: @perishable.errors, status: :unprocessable_entity
     end
   end
 
