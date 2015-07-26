69610a20f2436cc8773f5d107319a697c187f484
Marvin Qian <fueledbymarvin@gmail.com>
2015-07-25 22:22:14 -0400

    Rate limit to allow max 1 request/s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Throttle requests
+  config.middleware.use Rack::Throttle::Interval
 end
